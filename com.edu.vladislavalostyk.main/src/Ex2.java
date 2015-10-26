a1d982f774450d7383cc63f8a62cfb2b462a5e6e
vladislavalostyk <nesya.neska@yahoo.com>
2015-10-26 14:20:32 +0200

    Завдання на модуль номер 2

---
+++ com.edu.vladislavalostyk.main/src/Ex2.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Created by Сашка on 26.10.2015.
+ */
+public class Ex2 {
+    static float a, b, c;
+    static float d;
+    static double x1, x2;
+
+    public static void main(String args[]) throws IOException {
+        try {
+            System.out.println("Розв'язок квадратного рівняння");
+            Scanner s = new Scanner(System.in);
+            System.out.print("Введіть значення a -> ");
+            a = s.nextFloat();
+            System.out.print("Введіть значення b -> ");
+            b = s.nextFloat();
+            System.out.print("Введіть значення c -> ");
+            c = s.nextFloat();
+            System.out.println("a = " + a + " b = " + b + " c = " + c);
+
+            d = (b * b) - (4 * a * c);
+
+            System.out.println("Дискримінант = " + d);
+
+            if (d < 0)
+                System.out.print("Рівняння розв'язку немає");
+            else if (d == 0) {
+                x1 = (-b) / (2 * a);
+                System.out.println("x = " + x1);
+            } else {
+                x1 = (-b + Math.sqrt(d)) / (2 * a);
+                x2 = (-b - Math.sqrt(d)) / (2 * a);
+                if (x1 == x2)
+                    System.out.println("x1 = x2 " + x1);
+                else
+                    System.out.println("x1 =  " + x1 + "  x2 = " + x2);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Помилка вводу");
+        }
+    }
+}
