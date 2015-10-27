6d2d7d0a0fe049dce56018f0d6c16a21bf5dbfb9
vladislavalostyk <nesya.neska@yahoo.com>
2015-10-27 22:03:54 +0200

    Ex 5

--- com.edu.vladislavalostyk.main/src/Cafe.java
+++ com.edu.vladislavalostyk.main/src/Cafe.java
@@ -5,13 +5,13 @@ public class Cafe {
 
     public static void main(String[] args) {
 
-        Dish[] dishes = new Dish[6];
-        dishes[0] = new Dish(42, 508, "Голубки");
-        dishes[1] = new Dish(23, 487, "Борщик");
-        dishes[2] = new Dish(7, 114, "Узвар");
-        dishes[3] = new Dish(35, 617, "Вареники");
-        dishes[4] = new Dish(33, 988, "Крумплі");
-        dishes[5] = new Dish(9, 56, "Кавіль");
+        Ex5.Dish[] dishes = new Ex5.Dish[6];
+        dishes[0] = new Ex5.(42, 508, "Голубки");
+        dishes[1] = new Ex5.(23, 487, "Борщик");
+        dishes[2] = new Ex5.(7, 114, "Узвар");
+        dishes[3] = new Ex5.(35, 617, "Вареники");
+        dishes[4] = new Ex5.(33, 988, "Крумплі");
+        dishes[5] = new Ex5.(9, 56, "Кавіль");
 
         for (int i = 0; i < dishes.lenght; i++) {
             System.out.println(dishes[i]);
--- com.edu.vladislavalostyk.main/src/Ex5.java
+++ com.edu.vladislavalostyk.main/src/Ex5.java
@@ -2,7 +2,7 @@
  * Created by Сашка on 27.10.2015.
  */
 public class Ex5 {
-    public class Dish
+    public class Dish {
         int price;
         int calories;
         String name;
@@ -16,7 +16,7 @@ public class Ex5 {
 
         public String toString() {
             return String.format(
-                    " %9s страва\t, Ціна -: %5d,\t Калорійність %d ",
+                    " %9s страва\t, Ціна -: %5d,\t Калорійність %d "),
                     this.price, this.name, this.calories);
         }
 
