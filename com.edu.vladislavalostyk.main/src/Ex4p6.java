b344f5471a745f75f2ffb803615582e14805cb31
vladislavalostyk <nesya.neska@yahoo.com>
2015-10-27 22:21:41 +0200

    Ex 4p6

---
+++ com.edu.vladislavalostyk.main/src/Ex4p6.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Сашка on 27.10.2015.
+ */
+public class Ex4p6 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Enter ?:");
+
+        double x = scr.nextInt();
+        double d = 1 + x;
+        double sqrt = Math.sqrt(d);
+
+        System.out.println(sqrt);
+
+    }
+}
